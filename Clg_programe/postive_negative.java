148b9f08a52e670e6475e2fa3a3e6112d2b63875
Anjali Sharma <yourgithubemail@example.com>
2026-08-19 22:55:42 +0530

    Delete unnecessary file

---
+++ Clg_programe/postive_negative.java
@@ -0,0 +1,13 @@
+import  java.util.*;
+public  class  postive_negative{
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>0){
+              System.out.print("Positive");
+        }else{
+               System.out.print("Negative");
+        }
+          
+    }
+}
